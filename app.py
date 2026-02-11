02dd56951207c532652c8b8dc7c8426ede52f5b7
Chaitu-p-09 <chaitanya.payghan.aire@gmail.com>
2026-02-11 12:58:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 HF_API_KEY = os.environ.get("HF_API_KEY")
-MODEL = "mistralai/Mistral-7B-Instruct-v0.2"
+MODEL = "google/flan-t5-large"
 
 @app.route("/lyra", methods=["POST"])
 def lyra():
